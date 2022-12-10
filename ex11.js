0b5509e44a92035d2058323e2c0ed53ca5f1a1c4
Leigh Michael Forrest <leigh@catbirdseat.io>
2022-12-10 16:11:12 -0500

    ex11 third part completed

--- ex11.js
+++ ex11.js
@@ -21,3 +21,17 @@ printPet('Fran', 100, 'Crazy', 2);
 printPet('Alex', 30, 'Lizzy Lizard', 1);
 printPet("Eve", 35, 'Kong The Donkey', 20);
 
+
+// this part is tough! brain melting! give it a try
+//
+console.log('--------------- callback style ---------------');
+
+const fancyPet = (owner_name, owner_age, pet_name, pet_age, cb) => {
+    cb(owner_name, owner_age, pet_name, pet_age);
+    console.log(`That person owns ${pet_name} who is ${pet_age} years old.`);
+}
+
+// notice how I use a function here as the parameter cb?
+fancyPet('Zed', 44, 'Mr. Scruffles', 10, (name, age) => {
+    console.log(`Ooooh fancy ${name} you are ${age} years old.`);
+});
