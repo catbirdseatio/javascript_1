2c9bcee002e5b36f82c417139d1f88acc5242cec
Leigh Michael Forrest <leigh@catbirdseat.io>
2022-12-16 19:47:27 -0500

    ex31 completed

---
+++ ex31/index.js
@@ -0,0 +1,6 @@
+import { area, circumference} from '../geometry.js'
+
+let area51 = area(2.8);
+let circ2 = circumference(6);
+
+console.log(`Area: ${area51}, Circumference: ${circ2}`);
---
+++ geometry.mjs
@@ -0,0 +1,3 @@
+export const area = (r) => Math.PI * r ** 2;
+export const circumference = (r) => 2 * Math.PI * r;
+export const volume_cone = (r, h) => Math.PI * r ** 2 * (h / 3);
